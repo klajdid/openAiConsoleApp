b3dee43ffebd4e592c5580de96f5bcbf1650e4cf
klajdiD <klajdiducellari3@gmail.com>
2024-05-13 15:26:33 +0200

    init

---
+++ OpenAIConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OpenAI_API;
+using OpenAI_API.Completions;
+
+Console.WriteLine("Hello, World!");
+
+string openAiApiKey = "";
+string message = Console.ReadLine();
+string aiResponse = await OpenAi(message);
+Console.WriteLine(aiResponse);
+async Task<string> OpenAi(string userMessage)
+{
+    string outputResult = "";
+    var openai = new OpenAIAPI(openAiApiKey);
+    CompletionRequest completionRequest = new CompletionRequest();
+    completionRequest.Prompt = userMessage;
+    completionRequest.MaxTokens = 1024;
+
+    var completions = await openai.Completions.CreateCompletionAsync(completionRequest);
+
+    foreach (var completion in completions.Completions)
+    {
+        outputResult += completion.Text;
+    }
+    return outputResult;
+}
