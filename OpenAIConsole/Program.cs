9b117a8c34c5576d3fbd8e9b42f8fccc75b57672
joniH00 <jonihasimja@gmail.com>
2024-05-15 17:32:31 +0200

    change apikey data

--- OpenAIConsole/Program.cs
+++ OpenAIConsole/Program.cs
@@ -11,7 +11,7 @@ namespace OpenAIConsole
         static async Task Main(string[] args)
         {
             // Initialize Azure OpenAI service client
-            var client = new AssistantsClient(null);
+            var client = new AssistantsClient("");
 
             // Prompt the user to enter the file path
             Console.WriteLine("Please provide the path to the file:");
